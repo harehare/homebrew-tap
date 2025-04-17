0cdfc8785420524de9b8e03953a5242865e92294
harehare <harehare1110@gmail.com>
2025-04-17 23:54:45 +0900

    :sparkles: Add v0.1.1

--- Formula/mq@v0.1.0.rb
+++ Formula/mq@v0.1.0.rb
@@ -1,24 +1,22 @@
-class Mq < Formula
+class MqAt010 < Formula
   desc "A jq-like command-line tool for Markdown processing"
   homepage "https://github.com/harehare/mq"
-
-  version = "v0.1.1"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/harehare/mq/releases/download/#{version}/mq-x86_64-apple-darwin"
     sha256 "e17e4ca07a95ae68a17eb345c6d23515c5368920795d13f81fe3caaff928d41b"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/harehare/mq/releases/download/#{version}/mq-aarch64-apple-darwin"
-    sha256 "b8711ce7e91cce41655269d68212cb4cce45346bc3a124220403cb2d0e4d87b1"
+    sha256 "6ff4e6acbd261d821eb3fe1612e98cd5b390e257e14dd471fcd5579ae9ecffa3"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/harehare/mq/releases/download/#{version}/mq-x86_64-unknown-linux-gnu"
-    sha256 "b9e2c997c631dfa7fdec27f5e6beb3261720722e8df40ae1f7d8cf7ebf9036a1"
+    sha256 "14708af8614d0ef519c0ad0e97b7dd6397e0e851d42396012eac50e91686b5c2"
   elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/harehare/mq/releases/download/#{version}/mq-aarch64-unknown-linux-gnu"
-    sha256 "c49dc0cd617ece52c5b6f07ae76f6619420cfa9303d268aaea4ce084b60272e8"
+    sha256 "942640456140b3c92e99629f7691667d1aad38ffbf5966a5d54da53423ef2a39"
   end
 
-  version "0.1.1"
+  version "0.1.0"
   license "MIT"
 
   def install
